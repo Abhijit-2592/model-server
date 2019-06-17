2ec0293c7fee3a80d8737beca42eb67e2e81132f
abhijit <balaabhijit5@gmail.com>
2019-06-17 22:23:06 +0530

    single setup script to compile all protobufs including grpc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "compiling protobufs!"
 protoc --python_out=. model_server/protos/*.proto
 protoc --python_out=. model_server/apis/*.proto
 # compile gRPC server for python
-python -m grpc_tools.protoc -I=model_server --python_out=. --grpc_python_out=. model_server/server.proto
+python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. model_server/server.proto
+echo "Done!!!"
