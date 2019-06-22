b4422dbde2bd3d61e1a5b55904fec7a6bd6d8148
abhijit <balaabhijit5@gmail.com>
2019-06-22 16:58:43 +0530

    added about.py and version info

--- model_server/__init__.py
+++ model_server/__init__.py
@@ -1,1 +1,2 @@
 from .core import *
+from .about import __version__
---
+++ model_server/about.py
@@ -0,0 +1,5 @@
+__version__ = '0.1'
+__author__ = 'Abhijit Balaji'
+__title__ = 'Model Server'
+__summary__ = 'A Pure `python-3` based flexible gRPC server for hosting Deep Learning, Machine Learning models trained on any framework!'
+__email__ = 'balaabhijit5@gmail.com'
