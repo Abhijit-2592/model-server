74dc9ec3d4bec30577da926ad4db122bf4905479
abhijit <balaabhijit5@gmail.com>
2019-06-22 16:40:21 +0530

    added examples properly

--- examples/simple_servable/simple_servable.py
+++ examples/simple_servable/simple_servable.py
@@ -4,7 +4,7 @@ from model_server import Servable
 
 class my_custom_servable(Servable):
     def __init__(self, args):
-        print(args.check)
+        pass
 
     def predict(self, input_array_dict):
         print(input_array_dict)
