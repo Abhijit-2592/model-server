7a40e38dedbfd04705c723f6dd3570bba2ef11b1
Abhijit-2592 <balaabhijit5@gmail.com>
2025-08-05 16:47:26 -0700

    removed the legacy upload code

--- create_pip_wheel_and_upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bash ./compile_protobufs.sh
-echo "Creating pip wheel!"
-python3 setup.py sdist bdist_wheel
-echo "Wheel created"
-# make sure you have ~/.pypirc file
-echo "uploading to pypip server"
-python3 -m twine upload --verbose --repository-url https://upload.pypi.org/legacy/ dist/*
